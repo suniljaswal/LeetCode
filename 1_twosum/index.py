4083c901d11d6934b65e0a940865b7f84768d26c
suniljaswal <nothingelse007@gmail.com>
2022-10-09 15:12:38 +0530

    da

---
+++ 1_twosum/index.py
@@ -0,0 +1,11 @@
+class Solution:
+    def twoSum(self, nums, target): 
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
+nums = [2,7,11,15]
+target = 9
+a=Solution()
+b=a.twoSum(nums,target)
+print (b)
